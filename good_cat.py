27036ab36dc959a1658c26491935c26054df0b69
George Stalker <george.h.stalker.iv@gmail.com>
2024-09-26 23:17:20 -0400

    2

---
+++ good_cat.py
@@ -0,0 +1,6 @@
+GENRES = {
+    'cat_kids': {"children's game",},
+    'cat_war': {'modern warfare', 'wargame', 'american civil war', 'civil war', 'korean war',
+            'world war ii', 'american revolutionary war', 'vietnam war', 'american indian wars',
+            'world war i', },
+}
